be5c7241c7467e63471852fc97b1a237b5631822
Daniele Zago - ST <zagodaniele.9@gmail.com>
2023-05-01 20:37:46 -0400

    Added interfaces to package

--- src/SPM.jl
+++ src/SPM.jl
@@ -2,6 +2,8 @@ module SPM
 
 using Parameters
 
+include("phase1/interface.jl")
+include("nominal/interface.jl")
 include("statistics/interface.jl")
 include("limits/interface.jl")
 # include("charts/interface.jl")
