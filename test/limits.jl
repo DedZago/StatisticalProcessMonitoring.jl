2cf02c048a1f927cbd88060dd85b65d6f029ae80
Daniele Zago - ST <zagodaniele.9@gmail.com>
2023-05-01 20:38:06 -0400

    Testing limits

--- test/limits.jl
+++ test/limits.jl
@@ -18,5 +18,25 @@ using Test
         L = TwoSidedLimit([h for _ in 1:10])
         @test_throws AssertionError TwoSidedLimit([-h for _ in 1:5])
     end
+    @testset "isOC statistic" begin
+        STAT = EWMA(λ=0.2, value=1.0)
+        L1 = OneSidedLimit([1.5], [true])
+        @test is_IC(L1, STAT)
+        @test !is_OC(L1, STAT)
+        L2 = OneSidedLimit([0.5], [true])
+        @test is_OC(L2, STAT)
+        @test !is_IC(L2, STAT)
+        L3 = TwoSidedLimit(0.5)
+        @test is_OC(L3, STAT)
+        @test !is_IC(L3, STAT)
+        L4 = TwoSidedLimit(3.0)
+        @show get_value(STAT), get_value(L4)
+        @test is_IC(L4, STAT)
+        @test !is_OC(L4, STAT)
+        STAT = EWMA(λ=0.2, value=-1.0)
+        L5 = TwoSidedLimit(0.8)
+        @test !is_IC(L5, STAT)
+        @test is_OC(L5, STAT)
+    end
 end
 end
