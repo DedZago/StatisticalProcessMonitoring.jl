81139e39b15e78d0204491a04d3df2de62b9decf
Daniele Zago - ST <zagodaniele.9@gmail.com>
2023-07-01 18:48:43 -0400

    TODO: test residual statistics

---
+++ src/statistics/residual.jl
@@ -0,0 +1,26 @@
+abstract type ResidualStatistic <: AbstractStatistic end
+
+residual(x, S::ResidualStatistic) = error("Not implemented for abstract class")
+
+################ Control charts based on residuals ################
+
+#FIXME: test general interface for residual statistic
+get_statistic(S::ResidualStatistic) = S.stat
+get_value(S::ResidualStatistic) = get_value(get_statistic(S))
+get_design(S::ResidualStatistic) = get_design(get_statistic(S))
+set_design!(S::ResidualStatistic, par) = set_design!(get_statistic(S), par)
+get_maxrl(S::ResidualStatistic) = get_maxrl(get_statistic(S))
+update_statistic(S::ResidualStatistic, x) = update_statistic(get_statistic(S), residual(x, S))
+update_statistic!(S::ResidualStatistic, x) = update_statistic!(get_statistic(S), residual(x, S))
+
+@with_kw mutable struct LocationScaleStatistic{S, M, P} <: ResidualStatistic
+    stat::S
+    μ::M = 0.0
+    Ω::P = 0.0
+end
+export LocationScaleStatistic
+
+#FIXME: test whether the location scale constructors and the residual function work on vector and matrix data
+LocationScaleStatistic(stat, x::AbstractVector) = LocationScaleStatistic(stat, mean(x), 1.0/std(x))
+LocationScaleStatistic(stat, x::AbstractMatrix) = LocationScaleStatistic(stat, mean.(eachcol(x)), inv(sqrt(cov(x))))
+residual(x, S::LocationScaleStatistic) = S.Ω * (x .- S.μ)
