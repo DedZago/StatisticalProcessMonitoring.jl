62c250e50083501d3b1d318b5fbf4f6b007fd3cc
Daniele Zago - ST <zagodaniele.9@gmail.com>
2023-07-04 00:26:57 -0400

    Location-scale statistic

--- src/statistics/residual.jl
+++ src/statistics/residual.jl
@@ -13,11 +13,27 @@ get_maxrl(S::ResidualStatistic) = get_maxrl(get_statistic(S))
 update_statistic(S::ResidualStatistic, x) = update_statistic(get_statistic(S), residual(x, S))
 update_statistic!(S::ResidualStatistic, x) = update_statistic!(get_statistic(S), residual(x, S))
 
+"""
+    LocationScaleStatistic{S, M, P}
+
+A mutable struct representing a statistic applied to a location-scale family.
+
+# Fields
+- `stat::S`: The statistic.
+- `μ::M`: The location parameter.
+- `Ω::P`: The precision parameter (inverse of the variance).
+
+# Examples
+    STAT = EWMA(λ = 0.2)
+    RSTAT = LocationScaleStatistic(STAT, 1.0, 2.5)
+"""
 @with_kw mutable struct LocationScaleStatistic{S, M, P} <: ResidualStatistic
     stat::S
     μ::M = 0.0
     Ω::P = 0.0
 end
+
+
 export LocationScaleStatistic
 
 #FIXME: test whether the location scale constructors and the residual function work on vector and matrix data
--- src/statistics/stats-interface.jl
+++ src/statistics/stats-interface.jl
@@ -4,12 +4,23 @@ get_design(::AbstractStatistic) = @NamedTuple{}
 export get_design
 set_design!(::AbstractStatistic) = error("Not implemented for abstract class.") 
 export set_design!
+
+"""
+    get_value(stat::AbstractStatistic)
+
+Get the current value of a statistic.
+"""
 get_value(stat::AbstractStatistic) = stat.value
 export get_value
-update_statistic!(stat::AbstractStatistic, x) = error("Not implemented for abstract class.")
-export update_statistic!
+"""
+    update_statistic!(stat::AbstractStatistic, x)
+
+Update a statistic with a new observation x
+"""
 update_statistic(stat::AbstractStatistic, x) = error("Not implemented for abstract class.")
 export update_statistic
+update_statistic!(stat::AbstractStatistic, x) = error("Not implemented for abstract class.")
+export update_statistic!
 get_maxrl(::AbstractStatistic) = Inf
 get_maxrl(stat::Vector{T}) where T <: AbstractStatistic = minimum(get_maxrl.(stat))
 export get_maxrl
