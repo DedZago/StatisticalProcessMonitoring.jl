08558cf3fca4eadabf2ae883113dca31b110a669
Daniele Zago - ST <zagodaniele.9@gmail.com>
2023-07-21 19:58:53 -0400

    Temporary bias correction

--- src/algorithms/control-limit/double-bootstrap.jl
+++ src/algorithms/control-limit/double-bootstrap.jl
@@ -32,6 +32,7 @@ function doubleBootstrap!(CH::ControlChart; settings::OptSettings = OptSettings(
 
     #TODO: consider trunc_bi of the control chart run lengths when calculating the control limit?
 
+
     @unpack rlsim, maxiter_bi, nsims_bi, trunc_bi, x_tol_bi, f_tol_bi, verbose_bi = settings
 
     @assert maxiter_bi > 0 "maxiter_bi must be positive"
@@ -59,58 +60,17 @@ function doubleBootstrap!(CH::ControlChart; settings::OptSettings = OptSettings(
     i = 0
     conv = "Maximum number of iterations reached"
     target = get_nominal_value(CH)                  # Target nominal ARL/QRL/...
-    last_E_RL_estimate = target - (f_tol_bi + 1.0)
-    h = 0.0
-    while abs(last_E_RL_estimate - target) > f_tol_bi
-        hmax_bi = maximum(rl_paths)
-        hmin_bi = 0.0
-        if verbose_bi println("Running bisection on simulated paths with endpoints [$(hmin_bi), $(hmax_bi)] ...") end
-        hold = hmax_bi + x_tol_bi + 1.0                 # Starting value to assess convergence
-        RLs = Vector{Float64}(undef, nsims_bi_i)        # Vector of simulated run lenghts
-        E_RL = 0.0                                      # Estimated ARL/QRL/...
-        h = 0.0                                         # Initialize control limit value
-        idx = Vector{Int}(undef, nsims_bi_i)
-        rows = 1:nsims_bi_i
-        i = 0
-        while i < maxiter_bi
-            i = i+1
-            h = (hmin_bi + hmax_bi) / 2
-            if verbose_bi print("i: $(i)/$(maxiter_bi),\th: $(h)\t") end
 
-            idx .= sample(rows, nsims_bi_i)
-            # Calculate run length on simulated paths
-            set_h!(get_limit(CH), h)
-            for j in 1:nsims_bi_i
-                for k in 1:maxrl
-                    #FIXME: use set_h! and set_value! to make it more general to double-sided limits etc...
-                    set_value!(CH, rl_paths[idx[j], k])
-                    # @show get_value(CH), get_limit_value(CH)
-                    if is_OC(CH)
-                        RLs[j] = k
-                        break
-                    end 
-                    if k == maxrl
-                        RLs[j] = maxrl
-                    end
-                end
-            end
-            # Calculate nominal measure (ARL/QRL/...) 
-            E_RL = measure(RLs, CH, verbose=verbose_bi)
-            # Apply bisection algorithm
-            if E_RL > target
-                hmax_bi = h
-            else
-                hmin_bi = h
-            end
-            # Assess convergence in the run length value
-            if abs(E_RL - target) < f_tol_bi
-                conv = "Convergence (target)"
-                break
-            end
-            hold = h
-        end
-        last_E_RL_estimate = E_RL
+    #! Important
+    #FIXME: add parameters to control bootstrap-corrected control limit estimate
+    B = 1000
+    h_boot = zeros(B)
+    h = bisection_paths(deepcopy(CH), rl_paths, target, maxrl, nsims_bi_i, x_tol_bi, f_tol_bi, maxiter_bi, verbose_bi)
+    for b in 1:B
+        h_boot[b] = bisection_paths(deepcopy(CH), rl_paths[sample(1:nsims_bi_i, nsims_bi_i), :], target, maxrl, nsims_bi_i, x_tol_bi, f_tol_bi, maxiter_bi, false)
     end
+    h = 2*h - mean(h_boot)
+    # while abs(last_E_RL_estimate - target) > f_tol_bi
     set_value!(CH, old_value)
     set_h!(get_limit(CH), h)
     return (h=h, iter=i, status = conv)
@@ -136,3 +96,53 @@ function doubleBootstrap(CH::ControlChart; settings::OptSettings = OptSettings(i
 end
 export doubleBootstrap
 
+
+function bisection_paths(CH::ControlChart, rl_paths, target, maxrl, nsims_bi_i, x_tol_bi, f_tol_bi, maxiter_bi, verbose_bi)
+    hmax_bi = maximum(rl_paths)
+    hmin_bi = 0.0
+    if verbose_bi println("Running bisection on simulated paths with endpoints [$(hmin_bi), $(hmax_bi)] ...") end
+    hold = hmax_bi + x_tol_bi + 1.0                 # Starting value to assess convergence
+    RLs = Vector{Float64}(undef, nsims_bi_i)        # Vector of simulated run lenghts
+    E_RL = 0.0                                      # Estimated ARL/QRL/...
+    h = 0.0                                         # Initialize control limit value
+    idx = Vector{Int}(undef, nsims_bi_i)
+    rows = 1:nsims_bi_i
+    i = 0
+    while i < maxiter_bi
+        i = i+1
+        h = (hmin_bi + hmax_bi) / 2
+        if verbose_bi print("i: $(i)/$(maxiter_bi),\th: $(h)\t") end
+
+        idx .= sample(rows, nsims_bi_i)
+        # Calculate run length on simulated paths
+        set_h!(get_limit(CH), h)
+        for j in 1:nsims_bi_i
+            for k in 1:maxrl
+                set_value!(CH, rl_paths[idx[j], k])
+                # @show get_value(CH), get_limit_value(CH)
+                if is_OC(CH)
+                    RLs[j] = k
+                    break
+                end 
+                if k == maxrl
+                    RLs[j] = maxrl
+                end
+            end
+        end
+        # Calculate nominal measure (ARL/QRL/...) 
+        E_RL = measure(RLs, CH, verbose=verbose_bi)
+        # Apply bisection algorithm
+        if E_RL > target
+            hmax_bi = h
+        else
+            hmin_bi = h
+        end
+        # Assess convergence in the run length value
+        if abs(E_RL - target) < f_tol_bi
+            conv = "Convergence (target)"
+            break
+        end
+        hold = h
+    end
+    return h    
+end
