4ecddb127418dea6d353580569226ad1b582d611
Daniele Zago - ST <zagodaniele.9@gmail.com>
2023-08-23 17:04:28 -0400

    starting to generalize double bootstrap

--- src/algorithms/control-limit/double-bootstrap.jl
+++ src/algorithms/control-limit/double-bootstrap.jl
@@ -44,16 +44,9 @@ function approximateBisectionCL!(CH::ControlChart; rlsim::Function = run_path_si
     if verbose println("Generating $(nsims) run length paths ...") end
     maxrl_i = Int(trunc(maxrl))
     nsims_i = Int(nsims)                              # Number of simulated run lengts
-    rl_paths = Matrix{Float64}(undef, nsims_i, maxrl_i)    # Generated run length paths
-    if parallel
-        Threads.@threads for i in 1:nsims_i
-            rl_paths[i, :] = rlsim(CH, maxiter = maxrl)
-        end
-    else
-        for i in 1:nsims_i
-            rl_paths[i, :] = rlsim(CH, maxiter = maxrl)
-        end
-    end
+
+    error("Generalize rl_paths to a function to obtain matrix/tensor as needed")
+    rl_paths = _calculate_rl_paths(CH, nsims_i, maxrl_i, parallel)
 
     target = get_nominal_value(CH)                  # Target nominal ARL/QRL/...
     h, iter, conv = _bisection_paths(deepcopy(CH), rl_paths, target, maxrl_i, nsims_i, x_tol, f_tol, maxiter, B, verbose)
@@ -66,6 +59,35 @@ function approximateBisectionCL!(CH::ControlChart; rlsim::Function = run_path_si
     return (h=h, iter=iter, status = conv)
 end
 export approximateBisectionCL!
+
+function _calculate_rl_paths(CH::ControlChart, nsims_i, maxrl_i, parallel)
+    error("Not implemented yet")
+    rl_paths = Matrix{Float64}(undef, nsims_i, maxrl_i)    # Generated run length paths
+    if parallel
+        Threads.@threads for i in 1:nsims_i
+            rl_paths[i, :] = rlsim(CH, maxiter = maxrl)
+        end
+    else
+        for i in 1:nsims_i
+            rl_paths[i, :] = rlsim(CH, maxiter = maxrl)
+        end
+    end
+end
+
+
+function _calculate_rl_paths(CH::MultipleControlChart, nsims_i, maxrl_i, parallel)
+    error("Not implemented yet")
+    rl_paths = Matrix{Float64}(undef, nsims_i, maxrl_i, length(get_statistic(CH)))    # Generated run length paths
+    if parallel
+        Threads.@threads for i in 1:nsims_i
+            rl_paths[i, :] = rlsim(CH, maxiter = maxrl)
+        end
+    else
+        for i in 1:nsims_i
+            rl_paths[i, :] = rlsim(CH, maxiter = maxrl)
+        end
+    end
+end
 
 """
     approximateBisectionCL(CH::ControlChart; kw...)
@@ -141,3 +163,62 @@ function _bisection_paths(CH::ControlChart, rl_paths, target, maxrl, nsims_i, x_
     return (h, i, conv)
 end
 export _bisection_paths
+
+
+
+function _bisection_paths(CH::MultipleControlChart, rl_paths, target, maxrl, nsims_i, x_tol, f_tol, maxiter, B, verbose)
+    #TODO: multiply the maximum value by the asymptotic inflating factor for curved control limits
+    @assert length(size(rl_paths)) == 3 "rl_paths must be a 3-dimensional array <currently is <$(length(size(rl_paths)))>"
+    @assert size(rl_paths)[3] == length(get_statistic(CH)) "Must have a rl_path for each statistic. Got instead <$(size(rl_paths)[3])> and <$(length(get_statistic(CH)))>"
+
+    hmax = maximum(rl_paths)
+    hmin = 0.0
+    if verbose println("Running bisection on simulated paths with endpoints [$(hmin), $(hmax)] ...") end
+    hold = hmax + x_tol + 1.0                 # Starting value to assess convergence
+    RLs = Vector{Float64}(undef, B)        # Vector of simulated run lenghts
+    E_RL = 0.0                                      # Estimated ARL/QRL/...
+    h = 0.0                                         # Initialize control limit value
+    idx = Vector{Int}(undef, B)
+    rows = 1:nsims_i
+    i = 0
+    conv = "Maximum number of iterations reached"
+    while i < maxiter
+        i = i+1
+        h = (hmin + hmax) / 2
+        if verbose print("i: $(i)/$(maxiter),\th: $(h)\t") end
+
+        idx .= sample(rows, B)
+        # Calculate run length on simulated paths
+        set_h!(get_limit(CH), h)
+        for j in 1:B
+            set_t!(CH, 0)
+            for k in 1:maxrl
+                set_value!(CH, rl_paths[idx[j], k])
+                set_t!(CH, get_t(CH) + 1)
+                # @show get_value(CH), get_limit_value(CH), is_OC(CH)
+                if is_OC(CH)
+                    RLs[j] = k
+                    break
+                end 
+                if k == maxrl
+                    RLs[j] = maxrl
+                end
+            end
+        end
+        # Calculate nominal measure (ARL/QRL/...) 
+        E_RL = measure(RLs, CH, verbose=verbose)
+        # Apply bisection algorithm
+        if E_RL > target
+            hmax = h
+        else
+            hmin = h
+        end
+        # Assess convergence in the run length value
+        if abs(E_RL - target) < f_tol
+            conv = "Convergence"
+            break
+        end
+        hold = h
+    end
+    return (h, i, conv)
+end
