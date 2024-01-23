617cd7c5032008cddb633d3f2de37dc79e47806a
Daniele Zago - ST <zagodaniele.9@gmail.com>
2024-01-23 17:54:53 +0100

    Fix documentation typo

--- src/algorithms/interface.jl
+++ src/algorithms/interface.jl
@@ -73,7 +73,7 @@ Optimizes the design of a control chart `CH` using a specified optimization algo
 - `settings::OptSettings`: The optimization settings that control the optimization routine (default: `OptSettings(CH)`).
 
 ### Keyword Arguments
-- `optimizer::Symbol`: The optimization algorithm to use (default: `:LN_BOBYQA`).
+- `optimizer::Symbol`: The optimization algorithm to use (default: `:Grid`).
 - `solver::Symbol`: The root-finding algorithm to use for control limit estimation (default: `:Bootstrap`).
 - `hmax::Float64`: The maximum value of the control limit, used only when the solver is set to `:Bisection` (default: 100.0)
 - `kw...`: Additional keyword arguments to pass to the solver algorithm.
@@ -120,7 +120,7 @@ Optimizes the design of a control chart using a specified optimization algorithm
 - `settings::OptSettings`: The optimization settings that control the optimization routine (default: `OptSettings(CH)`).
 
 ### Keyword Arguments
-- `optimizer::Symbol`: The optimization algorithm to use (default: `:LN_BOBYQA`).
+- `optimizer::Symbol`: The optimization algorithm to use (default: `:Grid`).
 - `solver::Symbol`: The root-finding algorithm to use for control limit estimation (default: `:Bootstrap`).
 - `hmax::Float64`: The maximum value of the control limit, used only for the bisection algorithm (default: 100.0)
 - `kw...`: Additional keyword arguments to pass to the solver algorithm.
